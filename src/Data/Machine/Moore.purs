b8de9319ca5b6c2d9354535b78d45bbcc23e4c76
Matthew Leon <ml@matthewleon.com>
2018-02-18 11:20:21 -0500

    forEach and forever

--- src/Data/Machine/Moore.purs
+++ src/Data/Machine/Moore.purs
@@ -2,11 +2,13 @@ module Data.Machine.Moore where
 
 import Prelude
 
+import Control.Monad.Rec.Class (class MonadRec, Step(..), tailRecM)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
 import Data.Tuple (Tuple(..), swap)
 import Data.Unfoldable (class Unfoldable, unfoldr)
 
+-- TODO: linearity? Mutate the Maybe Tuple behind the scenes?
 newtype Moore s o = Moore (s -> Maybe (Tuple s o))
 
 derive instance newtypeMoore :: Newtype (Moore s o) _
@@ -35,3 +37,18 @@ toUnfoldable :: forall u s o. Unfoldable u => Moore s o -> s -> u o
 toUnfoldable m = unfoldr unfoldStep
   where
   unfoldStep b = swap <$> step b m
+
+forEach :: forall m a s o. MonadRec m => Moore s o -> (o -> m a) -> s -> m Unit
+forEach m f = tailRecM go
+  where
+  go s = case step s m of
+    Just (Tuple s' o) -> f o *> pure (Loop s')
+    Nothing           -> pure $ Done unit
+
+forever :: forall s o. (s -> Tuple s o) -> Moore s o
+forever = moore <<< map Just
+
+iterate :: forall s. (s -> Maybe s) -> Moore s s
+iterate = moore <<< map (map double)
+  where
+  double x = Tuple x x
